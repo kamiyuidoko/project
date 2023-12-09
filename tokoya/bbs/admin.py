a1ac9a9030658d1750d9407a03cdab95cd7c837c
kamiyuidoko <152796119+kamiyuidoko@users.noreply.github.com>
2023-12-09 14:15:38 +0900

    Delete tokoya directory

--- tokoya/bbs/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import TopicModel,GenreModel,ViewModel
-
-# Register your models here.
-
-admin.site.register(TopicModel)
-admin.site.register(GenreModel)
-admin.site.register(ViewModel)
-
